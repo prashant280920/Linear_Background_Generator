75a8c10a1973279189440aaea52350b43dbee762
prashant280920 <jayantmehta8447442229@gmail.com>
2020-05-13 16:09:26 +0530

    add input event

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+var input = document.getElementById("direction");
+var btun = document.querySelector("#button")
 function setgradient(){
 	body.style.background =
-	 "linear-gradient(to right,"
+	 "linear-gradient("+input.value
+	 +","
 	 +color1.value
 	 + ","
 	 +color2.value
@@ -13,5 +15,15 @@ function setgradient(){
 	 css.innerHTML=body.style.background+";";
 
 }
+function addlistenerafterclick(){
+	setgradient();
+} 
+function addlistenerafterkeypresss(event){
+	if(event.keyCode===13){
+		setgradient();
+	}
+}
+btun.addEventListener('click',addlistenerafterclick);
+input.addEventListener('keypress',addlistenerafterkeypresss)
 color1.addEventListener("input",setgradient);
 color2.addEventListener("input",setgradient);
